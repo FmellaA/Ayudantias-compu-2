2886cab573e34df7c8454b1d646537d94ef0048e
Fernanda Mella A <fer.mella.a@gmail.com>
2025-08-29 16:54:41 -0400

    comentarios del shor.py

--- Python-101/Shor.py
+++ Python-101/Shor.py
@@ -1,7 +1,7 @@
 #extrapolación del algoritmo de Shor
 #muestra el producto de numeros primos para un numero natural cualquiera
 num_primos = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]
-N = int(input("Ingrese un número entre 1 y 60: "))
+N = int(input("Ingrese un número entre 1 y 60: ")) #resultado A 
 
 producto = []
 for a in num_primos: 
@@ -14,7 +14,7 @@ for a in num_primos:
 #se deja una unica lista, para la primera lista de elementos que lo cumple, porque va a ser una combinación única. 
 
 P = []
-for i in producto: 
+for i in producto: # elimina los unos extras
     if not i==1: 
         P.append(i)
 P = P + [1]
@@ -24,6 +24,9 @@ P = P + [1]
 #      stg = f'{P[i]}'+ '*' + stg
 # stg = stg + f'{P[-1]}'
 # print(f'{stg} = {N}')
+
+#Armamos los dos factores B y C a partir de lo que esta en la lista P
+
 if len(P) == 6: 
     print(f"{P[0]*P[1]*P[2]}*{P[3]*P[4]*P[5]}") 
 elif len(P) == 5: 
